a5439cc730f9e669a4678358be95c0427f1ffda4
fillip1984 <fillip1984@gmail.com>
2023-10-21 21:31:19 -0400

    init colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,15 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      info: "hsla(206, 100%, 60%, 1)",
+      warn: "hsla(77, 92%, 60%,1)",
+      danger: "hsla(0, 99%, 64%, 1)",
+      success: "hsla(126, 83%, 68%, 1)",
+      white: "hsla(23, 39%, 85%, 1)",
+      black: "hsla(197, 7%, 21%, 1)",
+      grey: "hsla(197, 7%, 60%, 1)",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
