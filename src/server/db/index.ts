927f1f0c6b2e31c328c04808a826903e3c6e0d42
fillip1984 <fillip1984@gmail.com>
2025-12-22 19:59:05 -0500

    still trying to connect via ssl

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -12,7 +12,9 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn =
+  globalForDb.conn ??
+  postgres(env.DATABASE_URL, { prepare: false, ssl: "require" });
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
