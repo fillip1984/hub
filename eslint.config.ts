5609d2438cdad34a5e5147c2a22478e8614cd183
fillip1984 <fillip1984@gmail.com>
2026-09-13 19:10:00 -0400

    building and deploying from actions take 1

--- eslint.config.ts
+++ eslint.config.ts
@@ -15,6 +15,8 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".open-next/**",
+    ".sst/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
