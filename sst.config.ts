0cf27702cdf4d70e6ae3e6ea2b91fb2baf3c733b
fillip1984 <fillip1984@gmail.com>
2026-09-13 18:59:31 -0400

    working on deploying

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
 
+const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_SCHEMA = process.env.DATABASE_SCHEMA
+
+if (!DATABASE_URL || !DATABASE_SCHEMA) {
+  throw new Error(
+    "Missing required environment variables: DATABASE_URL or DATABASE_SCHEMA"
+  )
+}
+
 export default $config({
   app(input) {
     return {
@@ -8,19 +17,15 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
-    };
+    }
   },
   async run() {
-    // tags?
     new sst.aws.Nextjs("hub", {
       domain: "hub.illizen.com",
-      server: {
-        runtime: "nodejs22.x",
-      },
       environment: {
-        DATABASE_URL: process.env.DATABASE_URL ?? "unknown",
-        DATABASE_SCHEMA: process.env.DATABASE_SCHEMA ?? "public",
+        DATABASE_URL,
+        DATABASE_SCHEMA,
       },
-    });
+    })
   },
-});
+})
